451262afd2648decee2f0df295f5358d0369283e
sooyun <suy0226@naver.com>
2024-12-02 09:40:41 +0900

    path alias 추가

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+      '/@': path.resolve(__dirname, './src'),
+      '/@views': path.resolve(__dirname, './src/views'),
+      '/@components': path.resolve(__dirname, './src/components'),
+      '/@compositions': path.resolve(__dirname, './src/compositions'),
+      '/@router': path.resolve(__dirname, './src/router'),
+    },
+  },
   plugins: [vue()],
 })
