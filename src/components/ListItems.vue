7b8c48a1621942ed5c37b4f26933d4578f8c63d8
sooyun <suy0226@naver.com>
2024-12-05 15:55:30 +0900

    항목 삭제

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -1,6 +1,8 @@
 <template>
   <div v-for="(item, idx) in data" :key="item.id">
     {{ item.name }}
+    <img src="/@/assets/images/Delete.svg" width="20" height="20" 
+      @click="onRemoveItem(item.id)" />
   </div>
 </template>
 
@@ -11,13 +13,17 @@ export default {
 </script>
 
 <script setup>
-
+import { inject } from 'vue'
 const props = defineProps({
   data: {
     type: Array,
     default: []
   }
 })
-
+const removeItem = inject("removeItem")
+const onRemoveItem = (id) => {
+  if(!confirm('삭제하시겠습니까?')) return;
+  removeItem(id)
+}
 
 </script>
